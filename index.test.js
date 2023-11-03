447093bba87a1129f604d1856a9ac5538e90c140
Yunusade <yunus.adebayo@sky.uk>
2023-11-03 07:52:51 +0000

    Musician Test

--- index.test.js
+++ index.test.js
@@ -13,9 +13,16 @@ const seedMusician = require("./seedData");
 describe('./musicians endpoint', () => {
     // Write your tests here
     
-    
+    test("Testing Musician Endpoint", async () => {
+        const response = await request(app).get("/musicians");
+        expect(response.statusCode).toBe(200)
+    })
 
-
+    test("Testing Musician Response", async () => {
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text)[0]
+        expect(responseData.name).toBe("Mick Jagger")
+    })
 
 
     
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const app = require("./src/app");
-const { db } = require("./db/connection")
+const { db } = require("./db/connection");
+const { Musician } = require("./models");
 const port = 3000;
+
+
 
 app.listen(port, () => {
     db.sync();
